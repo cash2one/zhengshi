7b0521e1d7cc610458f54d81b90d7b6458222da8
yfjelley <yfjelley@gmail.com>
2015-10-09 16:47:50 +0800

    first commit

---
+++ bbs/forum/templatetags/settingsvalue.py
@@ -0,0 +1,34 @@
+__author__ = 'TianShuo'
+from django import template
+from ddbid import settings
+from ddbid import conf
+
+register = template.Library()
+
+
+'''
+usage:
+first
+    {% load settingsvalue %}
+then
+   {% settings_value "MAX_UPLOAD_SIZE" %}
+   or
+    {% settings_value "logoname" %}
+
+'''
+
+@register.simple_tag
+def settings_value(name):
+    return getattr(settings, name, "")
+
+@register.simple_tag
+def conf_value(name):
+    return getattr(conf, name, "")
+
+
+def phone_cut(name):
+    return ''.join([name[0:3],'****',name[-4:]]).strip()
+register.filter(phone_cut)
+
+
+
